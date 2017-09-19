34a25f1c41fd497c841a47705bc059bb32d967ce
ChenRenFong <31932763+ChenRenFong@users.noreply.github.com>
2017-09-19 11:49:21 +0800

    katherine

--- Sort.cpp
+++ Sort.cpp
@@ -4,42 +4,42 @@
 	#define M_PI 3.14159265358979323846
 #endif
 
-bool _sortByIncreasingPerimeter(Shape const *a,Shape const *b){
+bool doSortByIncreasingPerimeter(Shape const *a,Shape const *b){
 	return a->perimeter() < b->perimeter();
 }
 
 void Sort::sortByIncreasingPerimeter(std::list<Shape *> *shapeList) {
-	shapeList->sort(_sortByIncreasingPerimeter);
+	shapeList->sort(doSortByIncreasingPerimeter);
 }
 
-bool _sortByDecreasingPerimeter(Shape const *a,Shape const *b){
+bool doSortByDecreasingPerimeter(Shape const *a,Shape const *b){
 	return a->perimeter() > b->perimeter();
 }
 
 void Sort::sortByDecreasingPerimeter(std::list<Shape *> * shapeList) {
-	shapeList->sort(_sortByDecreasingPerimeter);
+	shapeList->sort(doSortByDecreasingPerimeter);
 }
 
-bool _sortByIncreasingArea(Shape const *a,Shape const *b){
+bool doSortByIncreasingArea(Shape const *a,Shape const *b){
 	return a->area() < b->area();
 }
 
 void Sort::sortByIncreasingArea(std::list<Shape *> * shapeList) {
-	shapeList->sort(_sortByIncreasingArea);
+	shapeList->sort(doSortByIncreasingArea);
 }
 
-bool _sortByDecreasingArea(Shape const *a,Shape const *b){
+bool doSortByDecreasingArea(Shape const *a,Shape const *b){
 	return a->area() > b->area();
 }
 
 void Sort::sortByDecreasingArea(std::list<Shape *> * shapeList) {
-	shapeList->sort(_sortByDecreasingArea);
+	shapeList->sort(doSortByDecreasingArea);
 }
 
-bool _sortByIncreasingCompactness(Shape const *a,Shape const *b){
-	return ((4*M_PI*a->area())/(a->perimeter()*a->perimeter())) < ((4*M_PI*b->area())/(b->perimeter()*b->perimeter()));
+bool doSortByIncreasingCompactness(Shape const *a,Shape const *b){
+	return ( (4*M_PI*a->area()) / (a->perimeter()*a->perimeter()) ) < ( (4*M_PI*b->area()) / (b->perimeter()*b->perimeter()) );
 }
 
 void Sort::sortByIncreasingCompactness(std::list<Shape *> * shapeList) {
-	shapeList->sort(_sortByIncreasingCompactness);
+	shapeList->sort(doSortByIncreasingCompactness);
 }
--- utSort.h
+++ utSort.h
@@ -7,16 +7,28 @@
 const double epsilon = 0.000001;
 
 TEST (Sort, sortByIncreasingPerimeter) {
+	vertex ori = {0, 0};
+	vertex a1 = {1, 2};
+	vertex a2 = {2, 1};
+	vertex b1 = {5, 10};
+	vertex b2 = {30, 0};
+	
+	Rectangle rec1(0, 0, 2, 4);
+	Rectangle rec2(0, 0, 15, 20);
 	Circle cir1(0, 0, 2);
-	Circle cir2(0, 0, 4);
-	Circle cir3(0, 0, 6);
+	Circle cir2(0, 0, 5);
+	Triangle tri1(ori, a1, a2);
+	Triangle tri2(ori, b1, b2);
 
 	std::list<Shape *> shapes;
+	shapes.push_back(&rec1);
+	shapes.push_back(&rec2);
 	shapes.push_back(&cir1);
 	shapes.push_back(&cir2);
-	shapes.push_back(&cir3);
-
-	double ans[3] = { 2*2*M_PI, 4*2*M_PI, 6*2*M_PI };
+	shapes.push_back(&tri1);
+	shapes.push_back(&tri2);
+	
+	double ans[6] = { tri1.perimeter(), rec1.perimeter(), cir1.perimeter(), cir2.perimeter(), tri2.perimeter(), rec2.perimeter() };
 	std::vector<double> youAns;
 	
 	Sort::sortByIncreasingPerimeter(&shapes);
@@ -25,22 +37,34 @@ TEST (Sort, sortByIncreasingPerimeter) {
 		youAns.push_back(oneShape->perimeter());		
 	}
 	
-	ASSERT_NEAR(ans[0], youAns[0], epsilon);
-	ASSERT_NEAR(ans[1], youAns[1], epsilon);
-	ASSERT_NEAR(ans[2], youAns[2], epsilon);
+	for(int i = 0 ; i <= 6 ;i++ ) {
+		ASSERT_NEAR(ans[i], youAns[i], epsilon);
+	}
 }
 
 TEST (Sort, sortByDecreasingPerimeter) {
+	vertex ori = {0, 0};
+	vertex a1 = {1, 2};
+	vertex a2 = {2, 1};
+	vertex b1 = {5, 10};
+	vertex b2 = {30, 0};
+	
+	Rectangle rec1(0, 0, 2, 4);
+	Rectangle rec2(0, 0, 15, 20);
 	Circle cir1(0, 0, 2);
-	Circle cir2(0, 0, 4);
-	Circle cir3(0, 0, 6);
+	Circle cir2(0, 0, 5);
+	Triangle tri1(ori, a1, a2);
+	Triangle tri2(ori, b1, b2);
 
 	std::list<Shape *> shapes;
+	shapes.push_back(&rec1);
+	shapes.push_back(&rec2);
 	shapes.push_back(&cir1);
 	shapes.push_back(&cir2);
-	shapes.push_back(&cir3);
-	
-	double ans[3] = { 6*2*M_PI, 4*2*M_PI, 2*2*M_PI };
+	shapes.push_back(&tri1);
+	shapes.push_back(&tri2);
+
+	double ans[6] = { rec2.perimeter(), tri2.perimeter(), cir2.perimeter(), cir1.perimeter(), rec1.perimeter(), tri1.perimeter() };
 	std::vector<double> youAns;
 
 	Sort::sortByDecreasingPerimeter(&shapes);
@@ -49,22 +73,34 @@ TEST (Sort, sortByDecreasingPerimeter) {
 		youAns.push_back(oneShape->perimeter());		
 	}
 	
-	ASSERT_NEAR(ans[0], youAns[0], epsilon);
-	ASSERT_NEAR(ans[1], youAns[1], epsilon);
-	ASSERT_NEAR(ans[2], youAns[2], epsilon);
+	for(int i = 0 ; i <= 6 ;i++ ) {
+		ASSERT_NEAR(ans[i], youAns[i], epsilon);
+	}
 }
 
 TEST (Sort, sortByIncreasingArea) {
+	vertex ori = {0, 0};
+	vertex a1 = {1, 2};
+	vertex a2 = {2, 1};
+	vertex b1 = {5, 10};
+	vertex b2 = {30, 0};
+	
+	Rectangle rec1(0, 0, 2, 4);
+	Rectangle rec2(0, 0, 15, 20);
 	Circle cir1(0, 0, 2);
-	Circle cir2(0, 0, 4);
-	Circle cir3(0, 0, 6);
+	Circle cir2(0, 0, 5);
+	Triangle tri1(ori, a1, a2);
+	Triangle tri2(ori, b1, b2);
 
 	std::list<Shape *> shapes;
+	shapes.push_back(&rec1);
+	shapes.push_back(&rec2);
 	shapes.push_back(&cir1);
 	shapes.push_back(&cir2);
-	shapes.push_back(&cir3);
-	
-	double ans[3] = { cir1.area(), cir2.area(), cir3.area() };
+	shapes.push_back(&tri1);
+	shapes.push_back(&tri2);
+
+	double ans[6] = { tri1.area(), rec1.area(), cir1.area(), cir2.area(), tri2.area(), rec2.area() };
 	std::vector<double> youAns;
 
 	Sort::sortByIncreasingArea(&shapes);
@@ -73,22 +109,34 @@ TEST (Sort, sortByIncreasingArea) {
 		youAns.push_back(oneShape->area());		
 	}
 	
-	ASSERT_NEAR(ans[0], youAns[0], epsilon);
-	ASSERT_NEAR(ans[1], youAns[1], epsilon);
-	ASSERT_NEAR(ans[2], youAns[2], epsilon);
+	for(int i = 0 ; i <= 6 ;i++ ) {
+		ASSERT_NEAR(ans[i], youAns[i], epsilon);
+	}
 }
 
 TEST (Sort, sortByDecreasingArea) {
+	vertex ori = {0, 0};
+	vertex a1 = {1, 2};
+	vertex a2 = {2, 1};
+	vertex b1 = {5, 10};
+	vertex b2 = {30, 0};
+	
+	Rectangle rec1(0, 0, 2, 4);
+	Rectangle rec2(0, 0, 15, 20);
 	Circle cir1(0, 0, 2);
-	Circle cir2(0, 0, 4);
-	Circle cir3(0, 0, 6);
+	Circle cir2(0, 0, 5);
+	Triangle tri1(ori, a1, a2);
+	Triangle tri2(ori, b1, b2);
 
 	std::list<Shape *> shapes;
+	shapes.push_back(&rec1);
+	shapes.push_back(&rec2);
 	shapes.push_back(&cir1);
 	shapes.push_back(&cir2);
-	shapes.push_back(&cir3);
+	shapes.push_back(&tri1);
+	shapes.push_back(&tri2);
 	
-	double ans[3] = { cir3.area(), cir2.area(), cir1.area() };
+	double ans[6] = { rec2.area(), tri2.area(), cir2.area(), cir1.area(), rec1.area(), tri1.area() };
 	std::vector<double> youAns;
 	
 	Sort::sortByDecreasingArea(&shapes);
@@ -97,35 +145,50 @@ TEST (Sort, sortByDecreasingArea) {
 		youAns.push_back(oneShape->area());		
 	}
 	
-	ASSERT_NEAR(ans[0], youAns[0], epsilon);
-	ASSERT_NEAR(ans[1], youAns[1], epsilon);
-	ASSERT_NEAR(ans[2], youAns[2], epsilon);
+	for(int i = 0 ; i <= 6 ;i++ ) {
+		ASSERT_NEAR(ans[i], youAns[i], epsilon);
+	}
 }
 
 TEST (Sort, sortByIncreasingCompactness) {
-	Circle cir1(0, 0, 2);
-	Circle cir2(0, 0, 4);
-	Circle cir3(0, 0, 6);
-
-	double ans[3] = { ((4*M_PI*cir3.area())/(cir3.perimeter()*cir3.perimeter())),
-					  ((4*M_PI*cir2.area())/(cir2.perimeter()*cir2.perimeter())),
-					  ((4*M_PI*cir1.area())/(cir1.perimeter()*cir1.perimeter())) };
-	std::vector<double> youAns;
+	vertex ori = {0, 0};
+	vertex a1 = {1, 2};
+	vertex a2 = {2, 1};
+	vertex b1 = {5, 10};
+	vertex b2 = {30, 0};
 	
+	Rectangle rec1(0, 0, 2, 4);
+	Rectangle rec2(0, 0, 15, 20);
+	Circle cir1(0, 0, 2);
+	Circle cir2(0, 0, 5);
+	Triangle tri1(ori, a1, a2);
+	Triangle tri2(ori, b1, b2);
+
 	std::list<Shape *> shapes;
+	shapes.push_back(&rec1);
+	shapes.push_back(&rec2);
 	shapes.push_back(&cir1);
 	shapes.push_back(&cir2);
-	shapes.push_back(&cir3);
+	shapes.push_back(&tri1);
+	shapes.push_back(&tri2);
+
+	double ans[6] = { ( (4*M_PI*tri2.area()) / (tri2.perimeter()*tri2.perimeter()) ),
+					  ( (4*M_PI*tri1.area()) / (tri1.perimeter()*tri1.perimeter()) ),
+					  ( (4*M_PI*rec1.area()) / (rec1.perimeter()*rec1.perimeter()) ),
+					  ( (4*M_PI*rec2.area()) / (rec2.perimeter()*rec2.perimeter()) ),
+					  ( (4*M_PI*cir1.area()) / (cir1.perimeter()*cir1.perimeter()) ),
+					  ( (4*M_PI*cir2.area()) / (cir2.perimeter()*cir2.perimeter()) )};
+	std::vector<double> youAns;
+	
+	Sort::sortByIncreasingCompactness(&shapes);
 	
 	for(Shape *oneShape : shapes ) {
 		youAns.push_back(((4*M_PI*oneShape->area())/(oneShape->perimeter()*oneShape->perimeter())));		
 	}
 	
-	ASSERT_NEAR(ans[0], youAns[0], epsilon);
-	ASSERT_NEAR(ans[1], youAns[1], epsilon);
-	ASSERT_NEAR(ans[2], youAns[2], epsilon);
-
-	Sort::sortByIncreasingCompactness(&shapes);
+	for(int i = 0 ; i <= 6 ;i++ ) {
+		ASSERT_NEAR(ans[i], youAns[i], epsilon);
+	}
 }
 
 #endif
