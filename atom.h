10b8986216ea4221133398ead85196580acc2acf
ChenRenFong <31932763+ChenRenFong@users.noreply.github.com>
2017-10-17 11:29:36 +0800

    hw3

--- atom.h
+++ atom.h
@@ -13,6 +13,25 @@ public:
   string type() const{ return _type; }
   string symbol() const{ return _symbol; }
   string value() const{ return symbol(); }
+  
+  bool match( Term &term ){
+	if ( term.type() == _type || term.type() == "Number" ) {
+	  if (_symbol == term.symbol()) {return true;}
+      else {return false;}
+	}
+	else {
+	  bool ret = term.assignable();
+      if (term.assignable() == true) {
+        term.setValue(_symbol);
+        term.setNotAssignable();
+      }
+	  else {
+        if (_value == term.value()) {return true;}
+        else {return false;}
+	  }
+      return ret;
+	}
+  }
 
   string _type = "Atom";
   string _symbol;
